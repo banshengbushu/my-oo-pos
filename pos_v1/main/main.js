c5d8a90426a72b891118782bd481ad2f85bd37fc
banshenbushu <wangdanna1995@outlook.com>
2016-07-20 00:18:46 +0800

    modified

--- pos_v1/main/main.js
+++ pos_v1/main/main.js
@@ -1,25 +1,49 @@
+/***buildCartItems****/
 let buildCartItems = (tags, allItems) => {
   let cartItems = [];
 
-  for(let tag of tags){
+  for (let tag of tags) {
     let splittedTags = tag.split('-');
     let barcode = splittedTags[0];
-    let itemCount = parseFloat(splittedTags[1] || 1);
-    let cartItem = cartItems.find((cartItem) => {
+    let count = parseFloat(splittedTags[1] || 1);
+    let cartItem = cartItems.find(cartItem => cartItem.item.barcode == barcode);
 
-      return cartItem.item.barcode == barcode;
-    });
     if (cartItem) {
       cartItem.count++;
     }
     else {
-      let item = allItems.find((item)=>{
-
-        return item.barcode ==  barcode;
-      });
-      cartItems.push({item: item, count: itemCount});
+      let item = allItems.find(item => item.barcode == barcode);
+      cartItems.push({item, count});
     }
   }
 
   return cartItems;
 };
+/***buildSubCartItems****/
+let buildSubCartItems = (cartItems, promotions) => {
+  return cartItems.map(cartItem => {
+    let promotionType = getPromotionType(cartItem.item.barcode, promotions);
+    let {subtotal, saved} = discount(cartItem, promotionType);
+
+    return {cartItem, subtotal, saved};
+  });
+};
+
+let getPromotionType = (barcode, promotions) => {
+  let promotion = promotions.find(promotion => promotion.barcodes.includes(barcode));
+  return promotion ? promotion.type : '';
+};
+
+let discount = (cartItem,promotionType) => {
+let freeItemCount = 0;
+  if (promotionType === 'BUY_TWO_GET_ONE_FREE') {
+    freeItemCount = parseInt(cartItem.count / 3);
+  }
+  let saved = freeItemCount * cartItem.item.price;
+  let subtotal = cartItem.count * cartItem.item.price -saved;
+  return {subtotal,saved};
+  };
+
+
+
+
